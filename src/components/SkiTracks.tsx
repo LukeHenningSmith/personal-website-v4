75cc4a351f2b155ce36604e518c0cb8f99090d71
Luke Henning-Smith <lukeusydwork@gmail.com>
2025-10-27 20:25:00 +1100

    Hide ski-tracks on small screens

--- src/components/SkiTracks.tsx
+++ src/components/SkiTracks.tsx
@@ -1,4 +1,19 @@
+import { useEffect, useState } from "react";
+
 export default function SkiTracks() {
+  const [showSkiTracks, setShowSkiTracks] = useState(true);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setShowSkiTracks(window.innerWidth >= 960);
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   const skiPath =
     "M 1000 200 C 1000 220 800 270 950 300 C 1000 310 920 450 850 500 C 800 550 920 600 900 670 C 890 700 790 750 1050 950";
 
@@ -11,8 +26,12 @@ export default function SkiTracks() {
         className="ski-svg"
       >
         <g className="track">
-          <path className="track-line" d={skiPath} />
-          <path className="track-line track-line--offset" d={skiPath} />
+          {showSkiTracks && (
+            <>
+              <path className="track-line" d={skiPath} />
+              <path className="track-line track-line--offset" d={skiPath} />
+            </>
+          )}
         </g>
       </svg>
     </div>
