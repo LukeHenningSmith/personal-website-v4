45bd0baf2f7577cd5784a8cc18851deaf37be2f5
Luke Henning-Smith <lukeusydwork@gmail.com>
2025-11-01 22:36:08 +1100

    Added academic transcript & dot points to education section

--- src/components/sections/Education.tsx
+++ src/components/sections/Education.tsx
@@ -1,4 +1,6 @@
+import { ExternalLink } from "lucide-react";
 import { FadeUp } from "../layout/FadeUp";
+import { Button } from "../ui/button";
 import { Section } from "../utility/Section";
 
 export function Education({ animationOffset }: { animationOffset?: number }) {
@@ -33,8 +35,49 @@ export function Education({ animationOffset }: { animationOffset?: number }) {
         <div className="flex gap-2">
           <div className="flex mx-1 w-[60px] align-middle justify-center"></div>
 
-          <div className="flex-1">
-            Grade: Honours Class I and the University Medal
+          <div className="flex-1 flex flex-col gap-2 mt-2">
+            <div>Grade: Honours Class I and the University Medal</div>
+            <div>
+              <div className="flex flex-col gap-2 text-muted-foreground">
+                <ul className="list-disc pl-5 flex flex-col gap-1">
+                  <li>1st in Software Engineering graduating class</li>
+                  <li>
+                    Dean's List of Excellence in Academic Performance (2019, 22,
+                    23)
+                  </li>
+                  <li>Academic Merit Prize (2022)</li>
+                  <li>
+                    Engineering Sydney Industry Placement Scholarship (ESIPS)
+                  </li>
+                  <li>Dalyell Scholar</li>
+                  <li>
+                    Thesis (93/100) on the Tracking of user interactions with
+                    interface component libraries across multi-app Fin-tech
+                    systems, a Joint Venture with CBA through the ESIPS research
+                    Scholarship.
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div className="mt-1">
+              <Button
+                variant={"outline"}
+                size={"sm"}
+                title="View academic transcript"
+                className="group relative cursor-pointer text-muted-foreground bg-transparent hover:text-blue-600 hover:bg-blue-600/10 hover:border-blue-600 dark:hover:text-blue-300 dark:bg-transparent dark:hover:bg-blue-600/10 dark:hover:border-blue-600 transition-colors duration-200 ease-in-out"
+                onClick={() => {
+                  const newWindow = window.open(
+                    "https://www.myequals.net/sharelink/f8269a17-4f0c-4a01-b1c1-30bfbab6ea71/10d20b81-93a9-4dfe-b905-bbf59cd6de24",
+                    "_blank"
+                  );
+                  if (newWindow) newWindow.opener = null;
+                }}
+              >
+                <ExternalLink />
+                View academic transcript
+              </Button>
+            </div>
           </div>
         </div>
       </FadeUp>
